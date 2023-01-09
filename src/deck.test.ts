479835e2558a353031ef00224493bf92539b2f0c
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-09 00:29:43 +0000

    working deck

--- src/deck.test.ts
+++ src/deck.test.ts
@@ -1,1 +1,24 @@
 import { createDeckObject } from './deck';
+import { it, describe, expect } from 'vitest';
+
+const differentSuits: string[] = ['♥', '♦', '♣', '♠'];
+const differentValues: {
+  face: { [name: string]: number | number[] }[];
+  number: number[];
+} = {
+  face: [{ King: 14 }, { Queen: 13 }, { Jack: 12 }, { Ace: [1, 11] }],
+  // face : ['King', 'Queen', 'Jack', 'Ace'],
+  number: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+};
+
+describe('What object is created by createDeck?', () => {
+  it('logs the values of differentValues to the console', () => {
+    console.log(Object.values(differentValues));
+    expect(1).toBe(1);
+  });
+
+  it('logs the cards produced by createDeckObject', () => {
+    console.log(createDeckObject());
+  });
+  // console.log(createDeckObject());
+});
--- src/deck.ts
+++ src/deck.ts
@@ -2,12 +2,12 @@
 
 const differentSuits: string[] = ['♥', '♦', '♣', '♠'];
 const differentValues: {
-  face: { [name: string]: number | number[] }[];
+  face: { [face: string]: number[] }[];
   number: number[];
 } = {
-  face: [{ King: 14 }, { Queen: 13 }, { Jack: 12 }, { Ace: [1, 11] }],
+  face: [{ King: [10, 14] }, { Queen: [10, 13] }, { Jack: [10, 12] }, { Ace: [1, 11] }],
   // face : ['King', 'Queen', 'Jack', 'Ace'],
-  number: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+  number: [10, 9, 8, 7, 6, 5, 4, 3, 2]
 };
 
 class Deck {
@@ -20,17 +20,37 @@ class Deck {
 
 class Card {
   public suit: string;
-  public value: string | number;
+  public value: { [face: string]: number | number[] } | number;
 
-  public constructor(suit: string, value: string | number) {
+  public constructor(
+    suit: string,
+    value: { [face: string]: number | number[] } | number
+  ) {
     this.suit = suit;
     this.value = value;
   }
 }
 
+/*
+createDeckObject():function
+
+returns a full list of cards in a 52 card deck when called as an object
+
+values will have the form of objects with {suit: value}
+
+to start map through suits
+create a key in the form of {[suit: string]: number|string}
+
+so every suit needs to have a map through
+*/
+
 export function createDeckObject() {
   return differentSuits.flatMap((suit) => {
-    return differentValues.map;
+    return Object.values(differentValues).flatMap((key) => {
+      return key.map((value) => {
+        return new Card(suit, value);
+      });
+    });
   });
 }
 
