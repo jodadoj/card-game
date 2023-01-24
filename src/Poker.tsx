1cd4aa3f6bb46d431a5c1f300ed7058f4af9d853
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-24 19:59:53 +0000

    further work on poker

--- src/Poker.tsx
+++ src/Poker.tsx
@@ -1,11 +1,22 @@
 import { IdCard } from './deck';
-// import { findSuit } from './findSuit.ts';
+import { findSuit } from './findSuit.ts';
 import { findHighValue, findWrappedValue } from './findValue';
 interface PokerProps {
   playerHands: IdCard[][];
 }
 
 export interface handScore {
+  highcard: number;
+  straight: number[];
+  flush: string[];
+  fourOf: number[];
+  fullHouse: number[];
+  twoPair: number[];
+  threeOf: number[];
+  pair: number[];
+}
+
+export interface handResult {
   highcard: number;
   straight: boolean;
   flush: boolean;
@@ -41,127 +52,125 @@ export function getWrappedValues(hand: IdCard[]): number[] {
   });
 }
 
-/*
-export function checkHand(hand:IdCard[], wrapAces:boolean=false):handScore {
+export function checkHand(hand: IdCard[], wrapAces = false): handResult {
+  const valueHash: Record<number, number> = {}; //create empty objects
+  const suitHash: Record<string, number> = {};
+  // let result:Record<string, number[]|string[]>={};
+  const result: handScore = {
+    highcard: -1,
+    straight: [],
+    flush: [],
+    fourOf: [],
+    fullHouse: [],
+    twoPair: [],
+    threeOf: [],
+    pair: []
+  };
 
-    const valueHash:Record<number, number>={}; //create empty objects
-    const suitHash:Record<string, number>={};
-    // let result:Record<string, number[]|string[]>={};
-    // const result:Record<string, number[]|string[]> = {
-    //     highcard: [],
-    //     straight: [],
-    //     flush: [],
-    //     fourOf: [],
-    //     fullHouse: [],
-    //     twoPair: [],
-    //     threeOf: [],
-    //     pair: []
-    // }
+  for (const card of hand) {
+    if (!valueHash[findHighValue(card)]) {
+      valueHash[findHighValue(card)] = 1;
+    } else {
+      valueHash[findHighValue(card)] += 1;
+    }
+    if (!suitHash[findSuit(card)]) {
+      suitHash[findSuit(card)] = 1;
+    } else {
+      suitHash[findSuit(card)] += 1;
+    }
+  }
+  for (const [key, value] of Object.entries(suitHash)) {
+    if (value === 5) {
+      // result['flush']=[];
+      result.flush.push(key);
+    }
+  }
+  for (const [key, value] of Object.entries(valueHash)) {
+    if (value === 2) {
+      // if(!result['pair']){
+      // result['pair']=[];
+      result.pair.push(Number(key));
+      // }
+    }
+    if (value === 3) {
+      // if(!result['threeOf']){
+      // result['threeOf']=[];
+      result.threeOf.push(Number(key));
+      // }
+    }
+    if (value === 4) {
+      // if(!result['fourOf']){
+      // result['fourOf']=[];
+      result.fourOf.push(Number(key));
+      // }
+    }
+  }
+  let prevValue = 0;
+  let isStraight = true;
+  //if the array included a 2, an Ace and a King
+  //maybe do typeof like typeof(valueHash[2]) === 'number'
+  if (wrapAces && valueHash[2] && valueHash[14] && valueHash[13]) {
+    const wrappedValues: number[] = getWrappedValues(hand).sort();
+    // if(!result['highcard']){
+    if (result.highcard == -1) {
+      // result['highcard']=[];
+      result.highcard = wrappedValues[4];
+    }
+    prevValue = wrappedValues[0] - 1;
+    for (const current of wrappedValues) {
+      if (current !== prevValue + 1) {
+        isStraight = false;
+      }
+      prevValue++;
+    }
+    if (isStraight) {
+      // if(!result['straight']){
+      // result['straight']=[];
+      result.straight.push(result.highcard);
+      // }
+    }
+  } else {
+    const sortedValues: number[] = getValues(hand).sort();
+    // if(!result['highcard']){
+    if (result.highcard == -1) {
+      // result['highcard']=[]
+      result.highcard = sortedValues[4];
+    }
+    prevValue = sortedValues[0] - 1;
+    for (const current of sortedValues) {
+      if (current !== prevValue + 1) {
+        isStraight = false;
+      }
+      prevValue++;
+    }
+    if (isStraight) {
+      // if(!result['straight']){
+      // result['straight']=[];
+      result.straight.push(result.highcard);
+      // }
+    }
+  }
 
-    for (const card of hand){
-        if (!valueHash[findHighValue(card)]){
-            valueHash[findHighValue(card)]=1;
-        }
-        else{
-            valueHash[findHighValue(card)]+=1;
-        }
-        if (!suitHash[findSuit(card)]){
-            suitHash[findSuit(card)]=1;
-        }
-        else{
-            suitHash[findSuit(card)]+=1;
-        }
-    }
-    for (const [key,value] of Object.entries(suitHash)){
-        if (value === 5){
-            // result['flush']=[];
-            result.flush.push(key);
-        }
-    }
-    for (const [key,value] of Object.entries(valueHash)){
-        if (value === 2){
-            // if(!result['pair']){
-                // result['pair']=[];
-                result.pair.push(key);
-            // }
-        }
-        if (value === 3){
-            if(!result['threeOf']){
-                result['threeOf']=[];
-                result.threeOf.push(key);
-            }
-        }
-        if (value === 4){
-            if(!result['fourOf']){
-                result['fourOf']=[];
-                result.fourOf.push(key);
-            }
-        }
-    }
-    let prevValue = 0;
-    let isStraight = true;
-    //if the array included a 2, an Ace and a King
-    //maybe do typeof like typeof(valueHash[2]) === 'number'
-    if (wrapAces && valueHash[2] && valueHash[14] && valueHash[13]){
-        const wrappedValues:number[] = getWrappedValues(hand).sort();
-        if(!result['highCard']){
-            result['highCard']=[];
-            result.highCard.push(wrappedValues[4]);
-        }
-        prevValue = wrappedValues[0]-1;
-        for (const current of wrappedValues){
-            if(current !== prevValue+1){
-                isStraight = false;
-            }
-            prevValue++;
-        }
-        if (isStraight){
-            if(!result['straight']){
-                result['straight']=[];
-                result.straight.push(result.highcard[0]);
-            }
-        }
-    }
-    else{
-        const sortedValues:number[] = getValues(hand).sort();
-        if(!result['highCard']){
-            result['highCard']=[]
-            result.highCard.push(wrappedValues[4]);
-        }
-        prevValue = sortedValues[0]-1;
-        for (const current of sortedValues){
-            if(current !== prevValue+1){
-                isStraight = false;
-            }
-            prevValue++;
-        }
-        if (isStraight){
-            if(!result['straight']){
-                result['straight']=[];
-                result.straight.push(result.highcard[0]);
-            }
-        }
-    }
+  console.table(result);
 
-    return {
-        highcard: result.highCard[0],
-        straight: (typeof(result['straight'])==="number"),
-        flush: (typeof(result['flush'])==="number"),
-        fourOf: (typeof(result['fourOf'])==="number"),
-        fullHouse: (typeof(result['fullHouse'])==="number"),
-        twoPair: (typeof(result['twoPair'])==="number"),
-        threeOf: (typeof(result['threeOf'])==="number"),
-        pair:  (typeof(result['pair'])==="number" && result.pair===1)
-    }
+  return {
+    highcard: result.highcard,
+    straight: result.straight.push() !== undefined,
+    flush: result.flush.push() !== undefined,
+    fourOf: result.fourOf.push() !== undefined,
+    fullHouse: result.fullHouse.push() !== undefined,
+    twoPair: result.twoPair.push() !== undefined,
+    threeOf: result.threeOf.push() !== undefined,
+    pair: result.pair.push() !== undefined && result.pair.push() !== 1
+  };
 }
-*/
 
 export function Poker(props: PokerProps): JSX.Element {
   const playerHands: IdCard[][] = props.playerHands;
 
   const handScore: string[] = [];
   for (const hand in playerHands) {
-    handScore[hand] = isFlush(playerHands[hand]) ? 'Flush' : '';
+    handScore[hand] = checkHand(playerHands[hand]) ? 'Flush' : '';
   }
 
   //sort by correctId then see if the last id matches the next or is ascending/descending
@@ -317,18 +326,3 @@ function checkHand(hand:IdCard[], wrapAces:boolean=false):string{
 }
 
 */
-
-// function twoSums(nums: number[], target: number): [number, number] {
-// 	let hash:Record<number, number>={}
-
-// 	for(let i = 0; i < nums.length; i++) {
-// 		const n = nums[i];
-// 		if(hash[target - n] !== undefined) {
-// 			return [hash[target - n], i];
-// 		}
-// 		hash[n] = i;
-// 	}
-// 	return [-1, -1];
-//   };
-
-// function
